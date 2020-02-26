1d492e34c38e0f8a8778c2f8a732440d53c241f6
Sujit Jadhav <jadhav.sujit4@gmail.com>
2020-02-26 20:18:41 +0000

    Modified Analyzer for better results : 0.4200

--- src/main/java/com/lucene/indexandsearch/utils/Constants.java
+++ src/main/java/com/lucene/indexandsearch/utils/Constants.java
@@ -52,6 +52,8 @@ public class Constants {
     public static final String MODELLMJ = "LMJ";
     public static final String MODELLMD = "LMD";
 
+    public static final int lengthFilterMinimumLength = 3;
+    public static final int lengthFilterMaximumLength = 25;
 
 //    public static Analyzer makeAnalyzer() throws IOException {
 //        List<String> stopWordList = Arrays.asList("a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves");
