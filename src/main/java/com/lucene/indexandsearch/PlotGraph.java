915227c7ee3192037ad939fa072b0ed489e76d47
Sujit Jadhav <jadhav.sujit4@gmail.com>
2020-02-26 17:26:19 +0000

    Added idea PvsR graph

--- src/main/java/com/lucene/indexandsearch/PlotGraph.java
+++ src/main/java/com/lucene/indexandsearch/PlotGraph.java
@@ -18,13 +18,29 @@ public class PlotGraph {
 
         RunTrecEval.runTrec();
         Map<Integer, Float> trecXYValues =  RunTrecEval.trecEvalXYValues;
-        final XYSeries trecEvalMatrixValues = new XYSeries("");
+        final XYSeries trecEvalMatrixValues = new XYSeries("My Search Engine Graph :(");
         for (int i = 0; i < trecXYValues.size(); i++) {
             trecEvalMatrixValues.add(i, trecXYValues.get(i));
         }
+        final XYSeries idealPrecisionRecall = new XYSeries("Ideal Search Engine Graph");
+        idealPrecisionRecall.add(0, 0.85);
+        idealPrecisionRecall.add(1, 0.85);
+        idealPrecisionRecall.add(2, 0.84);
+        idealPrecisionRecall.add(3, 0.84);
+        idealPrecisionRecall.add(4, 0.83);
+        idealPrecisionRecall.add(5, 0.82);
+        idealPrecisionRecall.add(6, 0.81);
+        idealPrecisionRecall.add(7, 0.79);
+        idealPrecisionRecall.add(8, 0.75);
+        idealPrecisionRecall.add(9, 0.69);
+        idealPrecisionRecall.add(9.3,0.64);
+        idealPrecisionRecall.add(9.6,0.45);
+        idealPrecisionRecall.add(9.8,0.25);
+        idealPrecisionRecall.add(10,0.1);
 
         final XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(trecEvalMatrixValues);
+        dataset.addSeries(idealPrecisionRecall);
 
         JFreeChart xylineChart = ChartFactory.createXYLineChart(
                 "Precision VS Recall Curve",
