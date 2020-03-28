87474f546209d9bb939fc670ce76633f2dddc4b5
Chetan-Stark <cprasad@tcd.ie>
2020-03-28 17:37:00 +0000

    FR94 files index againnn!

---
+++ src/main/java/com/lucene/indexandsearch/fr94/FR94Data.java
@@ -0,0 +1,220 @@
+package com.lucene.indexandsearch.fr94;
+
+public class FR94Data {
+    public String docno;
+    public String parent;
+    public String text;
+    public String doctitle;
+    public String usdept;
+    public String agency;
+    public String usbureau;
+    public String address;
+    public String further;
+    public String summary;
+    public String action;
+    public String signer;
+    public String signjob;
+    public String supplem;
+    public String billing;
+    public String frfiling;
+    public String date;
+    public String cfrno;
+    public String rindock;
+    public String table;
+    public String footnote;
+    public String footcite;
+    public String footname;
+    public String all;
+
+    public String getDocno() {
+        return docno;
+    }
+
+    public void setDocno(String docno) {
+        this.docno = docno;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDoctitle() {
+        return doctitle;
+    }
+
+    public void setDoctitle(String doctitle) {
+        this.doctitle = doctitle;
+    }
+
+    public String getUsdept() {
+        return usdept;
+    }
+
+    public void setUsdept(String usdept) {
+        this.usdept = usdept;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public String getUsbureau() {
+        return usbureau;
+    }
+
+    public void setUsbureau(String usbureau) {
+        this.usbureau = usbureau;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFurther() {
+        return further;
+    }
+
+    public void setFurther(String further) {
+        this.further = further;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getSigner() {
+        return signer;
+    }
+
+    public void setSigner(String signer) {
+        this.signer = signer;
+    }
+
+    public String getSignjob() {
+        return signjob;
+    }
+
+    public void setSignjob(String signjob) {
+        this.signjob = signjob;
+    }
+
+    public String getSupplem() {
+        return supplem;
+    }
+
+    public void setSupplem(String supplem) {
+        this.supplem = supplem;
+    }
+
+    public String getBilling() {
+        return billing;
+    }
+
+    public void setBilling(String billing) {
+        this.billing = billing;
+    }
+
+    public String getFrfiling() {
+        return frfiling;
+    }
+
+    public void setFrfiling(String frfiling) {
+        this.frfiling = frfiling;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCfrno() {
+        return cfrno;
+    }
+
+    public void setCfrno(String cfrno) {
+        this.cfrno = cfrno;
+    }
+
+    public String getRindock() {
+        return rindock;
+    }
+
+    public void setRindock(String rindock) {
+        this.rindock = rindock;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getFootnote() {
+        return footnote;
+    }
+
+    public void setFootnote(String footnote) {
+        this.footnote = footnote;
+    }
+
+    public String getFootcite() {
+        return footcite;
+    }
+
+    public void setFootcite(String footcite) {
+        this.footcite = footcite;
+    }
+
+    public String getFootname() {
+        return footname;
+    }
+
+    public void setFootname(String footname) {
+        this.footname = footname;
+    }
+
+    public String getAll() {
+        return all;
+    }
+
+    public void setAll(String all) {
+        this.all = all;
+    }
+}
